1bf6fa23fef48eaa7c5adb28a19f5b013521ac46
deathy <deathy.epl.ccs@gmail.com>
2022-07-13 01:48:51 -0700

    Started work on implementing factory presets, but it requires more effort than I want to expend right now.

--- Source/SharedProperties.cpp
+++ Source/SharedProperties.cpp
@@ -7,7 +7,9 @@ using namespace juce;
 SharedProperties::SharedProperties ()
     :
     PropertiesFile (
-                    GetOptions () ) { InitializeProperties (); }
+                    GetOptions () )
+  , factory (
+             GetOptions () ) { InitializeProperties (); }
 
 auto
     SharedProperties::GetPatches () const -> XmlElement*
@@ -55,12 +57,73 @@ void
     }
 }
 
+void
+    SharedProperties::MergePatches ()
+{
+    auto left = factory . getXmlValue (
+                                       "patches" );
+    auto right = getXmlValue (
+                              " patches" );
+
+    merged = std::make_shared < XmlElement > (
+                                              "patches" );
+
+    merged -> addChildElement (
+                               new XmlElement (
+                                               "basses" ) );
+    merged -> addChildElement (
+                               new XmlElement (
+                                               "drums" ) );
+    merged -> addChildElement (
+                               new XmlElement (
+                                               "leads" ) );
+    merged -> addChildElement (
+                               new XmlElement (
+                                               "keys" ) );
+    merged -> addChildElement (
+                               new XmlElement (
+                                               "pads" ) );
+    merged -> addChildElement (
+                               new XmlElement (
+                                               "sfx" ) );
+    merged -> addChildElement (
+                               new XmlElement (
+                                               "misc" ) );
+    for ( auto i = 0 ; i < merged -> getNumChildElements () ; i++ )
+    {
+        const auto& category = merged -> getChildElement (
+                                                          i ) -> getTagName ();
+        const auto& leftCategory = left -> getChildByName (
+                                                           category );
+        for ( auto l = 0 ; l < leftCategory -> getNumChildElements () ; l++ )
+        {
+            const auto& patch = leftCategory -> getChildElement (
+                                                                 l );
+            // Thinking about it, this is a terrible way to do this because the
+            // files are independently updateable, so merging them statically
+            // will just make patches stop saving.
+            //
+            // Abandoning the effort for the time being, as this requires more thought to approach.
+        }
+    }
+}
+
 auto
     SharedProperties::GetOptions () -> Options
 {
     Options props;
     props . applicationName  = "SIDStep 2";
     props . filenameSuffix   = ".bank";
+    props . commonToAllUsers = false;
+    return props;
+}
+
+auto
+    SharedProperties::GetFactoryOptions () -> Options
+{
+    Options props;
+    props . applicationName  = "SIDStep 2";
+    props . filenameSuffix   = ".factory";
     props . commonToAllUsers = false;
     return props;
 }
--- Source/SharedProperties.h
+++ Source/SharedProperties.h
@@ -16,6 +16,15 @@ private:
     void
         InitializeProperties ();
 
+    void
+        MergePatches ();
+
     static auto
         GetOptions () -> Options;
+
+    static auto
+        GetFactoryOptions () -> Options;
+
+    PropertiesFile                 factory;
+    std::shared_ptr < XmlElement > merged;
 };
